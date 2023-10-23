d315c728c53ff29735af6e189093db154bb8dcfe
DiegoLonguechaux <diego.longuechaux@gmail.com>
2023-10-23 09:29:13 +0200

    changes

--- sitesae/src/pages/AccountPage.js
+++ sitesae/src/pages/AccountPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import skateImage from '../skate.png';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 const AccountPage = () => {
   return (
@@ -37,7 +38,9 @@ const AccountPage = () => {
               <input type="password" id="password" className="w-full p-2 border rounded" />
             </div>
             <div className="flex justify-end">
-              <button className="bg-black text-white px-4 py-2 rounded-lg mr-2">Save</button>
+              <Link to="/game-training">
+                <button className="bg-black text-white px-4 py-2 rounded-lg mr-2">Save</button>
+              </Link>
               <button className="bg-yellow-500 text-black px-4 py-2 rounded-lg">Delete account</button>
             </div>
           </div>
