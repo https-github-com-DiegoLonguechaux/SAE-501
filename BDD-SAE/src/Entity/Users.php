2b9d6cfaa850606cfe8e6e959ecfd4bc965f5aa7
DiegoLonguechaux <diego.longuechaux@gmail.com>
2023-10-27 11:53:36 +0200

    add api on entity users

--- BDD-SAE/src/Entity/Users.php
+++ BDD-SAE/src/Entity/Users.php
@@ -7,8 +7,10 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
+#[ApiResource]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
